ad69b7737dad91948358f0cec761c2340c8c502b
Yuko <yuko.mazurek@gmail.com>
2018-11-24 10:59:56 -0800

    Updates

---
+++ precache-manifest.c0f2f5d9c2045c996ce1b81e07aca5d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "918f57cd3cbb989a67ef",
+    "url": "/music-and-lyrics/static/js/runtime~main.918f57cd.js"
+  },
+  {
+    "revision": "1196e74d3bc84d12f798",
+    "url": "/music-and-lyrics/static/js/main.1196e74d.chunk.js"
+  },
+  {
+    "revision": "1b6164f145d35fbbd104",
+    "url": "/music-and-lyrics/static/js/1.1b6164f1.chunk.js"
+  },
+  {
+    "revision": "1196e74d3bc84d12f798",
+    "url": "/music-and-lyrics/static/css/main.ec14eb1f.chunk.css"
+  },
+  {
+    "revision": "1b6164f145d35fbbd104",
+    "url": "/music-and-lyrics/static/css/1.200183b0.chunk.css"
+  },
+  {
+    "revision": "c22aac432a94f0e3a857b61f7b2ed8d4",
+    "url": "/music-and-lyrics/index.html"
+  }
+];
