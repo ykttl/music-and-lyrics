8acc497457c69ffe87c996f5f041a491f643d7d3
Yuko <yuko.mazurek@gmail.com>
2019-12-28 16:48:53 +0900

    Updates

---
+++ precache-manifest.0e489b960668e811fec9bb7b7dd1e216.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5df634b7afae29e2da9cdd780f199a6",
+    "url": "/music-and-lyrics/index.html"
+  },
+  {
+    "revision": "893f585dcf7ee58247a2",
+    "url": "/music-and-lyrics/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "3268c5c6b5473d3c8a0c",
+    "url": "/music-and-lyrics/static/css/main.07531887.chunk.css"
+  },
+  {
+    "revision": "893f585dcf7ee58247a2",
+    "url": "/music-and-lyrics/static/js/2.cc6ab9ad.chunk.js"
+  },
+  {
+    "revision": "adf315857185901d18c2339ec58b6b66",
+    "url": "/music-and-lyrics/static/js/2.cc6ab9ad.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3268c5c6b5473d3c8a0c",
+    "url": "/music-and-lyrics/static/js/main.0e8be026.chunk.js"
+  },
+  {
+    "revision": "16061b58cb6f71a13bb6",
+    "url": "/music-and-lyrics/static/js/runtime-main.e9efff46.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/music-and-lyrics/precache-manifest.85c49594e97c7cabbaa3050647f1cf9e.js"
+  "/music-and-lyrics/precache-manifest.0e489b960668e811fec9bb7b7dd1e216.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/music-and-lyrics/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/music-and-lyrics/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
